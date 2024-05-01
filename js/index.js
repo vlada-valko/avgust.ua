977a943ec0c000b2761c3ad771878b21a87c253c
Vladyslava Valko <132363872+vlada-valko@users.noreply.github.com>
2024-05-01 13:32:23 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@ function init() {
   import("./menu.js");
   import("./counter.js");
 }
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
