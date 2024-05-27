64211400d3d402ebd041d3a3f5a0f36260bc8ce5
Vladyslava Valko <vlada.valko111@gmail.com>
2024-05-27 19:55:39 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 
-
 function init() {
-  import("./menu.js");
-  import("./index.carousel.js");
-  import("./counter.js");
-
-
-
+  Promise.all([
+      import('./menu.js'),
+      import('./counter.js'),
+      import('./index.carousel.js')
+  ]).then(([menu, counter, carousel]) => {
+      // Ви можете викликати функції з імпортованих модулів, якщо необхідно
+      // menu.initMenu();
+      // counter.initCounter();
+      // carousel.initCarousel();
+  }).catch(err => {
+      console.error('Error loading modules:', err);
+  });
 }
+
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
 let loadedPartialsCount = 0;
 
 document.body.addEventListener('htmx:afterOnLoad', () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) init();
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) {
+      init();
+  }
 });
