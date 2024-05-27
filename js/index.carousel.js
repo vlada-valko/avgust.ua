3eb624ed684824a6b31afed873505c3ccc00319e
Vladyslava Valko <vlada.valko111@gmail.com>
2024-05-27 14:33:50 +0300

    carousel102

--- js/index.carousel.js
+++ js/index.carousel.js
@@ -1,27 +1,31 @@
+function initSlick() {
+    var windowWidth = $(window).width();
+    var slidesToShow = windowWidth >= 1150 ? 3 : (windowWidth >= 850 ? 2 : 1);
 
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: slidesToShow,
+        autoplay:true,
+        speed:1000,
+        autoplaySpeed:800000,
+        responsive:[
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow:2
+                }
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+}
 
-$(window).on('load', function() {
-	$('.slider').slick({
-		arrows:true,
-		dots:true,
-		slidesToShow:3,
-		autoplay:true,
-		speed:1000,
-		autoplaySpeed:800000,
-		responsive:[
-			{
-				breakpoint: 1150,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 850,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
-	});
+$(window).on('load resize', function() {
+    initSlick();
 });
-
