dcf1fd6cc90d4e6c44446202b814a2d156a4ae04
Vladyslava Valko <vlada.valko111@gmail.com>
2024-05-31 14:45:42 +0300

    uodate json

--- js/index.js
+++ js/index.js
@@ -2,12 +2,11 @@
 
 function init() {
   import("./menu.js");
-  import("./counter.js");
   import("./index.carousel.js");
+  import("./counter.js");
   import("./our-culture-accordeon.js");
-  import("./download-btn.js");
-  import("./elastic-search.js");
   import("./our-team-import.js");
+
 
 }
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
