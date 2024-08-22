8f0adf97cc433fa2be86aa7df431ecf4b3691a48
Vladyslava Valko <vlada.valko111@gmail.com>
2024-08-22 16:29:12 +0300

    refactor files of edu

--- js/create-edu-page.js
+++ js/create-edu-page.js
@@ -1,14 +1,7 @@
+
 const username = 'vlada-valko';
 const repo = 'avgust.ua';
-
-function getCurrentFilePath() {
-    const path = window.location.pathname; // Отримати шлях поточної URL
-    const fileName = path.substring(path.lastIndexOf('/') + 1); // Видобути ім'я файлу
-    return decodeURIComponent(fileName.split('.')[0].toLowerCase()); // Видалити розширення, декодувати і перевести в нижній регістр
-}
-
-const fileName = getCurrentFilePath();
-const rootPath = `матеріали навчання`;
+const rootPath = 'матеріали-навчання';
 const apiUrl = `https://api.github.com/repos/${username}/${repo}/contents/${rootPath}`;
 
 function fetchWithCache(url) {
@@ -30,27 +23,69 @@ function fetchWithCache(url) {
         });
 }
 
-function processAllData(data) {
+function getConfig() {
+    const configElement = document.getElementById('folders-config');
+    if (!configElement) {
+        console.error('Element #folders-config not found');
+        return { folders: {}, files: [] };
+    }
+
+    return JSON.parse(configElement.textContent);
+}
+
+function formatFileName(fileName) {
+    // Видаляємо цифри перед крапкою
+    return fileName.replace(/^\d+\.\s*/, '');
+}
+
+function processData(data) {
     const contentContainer = document.querySelector('.accordion-container');
     if (!contentContainer) {
         console.error('Element .accordion-container not found');
         return;
     }
 
-    const folders = data.filter(item => item.type === 'dir');
-    folders.forEach(folder => {
-        const folderApiUrl = `https://api.github.com/repos/${username}/${repo}/contents/${rootPath}/${folder.name}`;
-        
-        // Отримати файли в папці
-        fetchWithCache(folderApiUrl)
+    const config = getConfig();
+    const folderConfigs = config.folders;
+    const fileNames = config.files;
+
+    // Create a map of folder names to their API URLs
+    const folderApiUrls = Object.keys(folderConfigs).reduce((urls, folderName) => {
+        const folder = data.find(item => item.type === 'dir' && item.name === folderName);
+        if (folder) {
+            urls[folderName] = `https://api.github.com/repos/${username}/${repo}/contents/${rootPath}/${folderName}`;
+        }
+        return urls;
+    }, {});
+
+    // Process folders according to config
+    const folderPromises = Object.keys(folderConfigs).map(folderName => {
+        const url = folderApiUrls[folderName];
+        const folderConfig = folderConfigs[folderName];
+        return fetchWithCache(url)
             .then(files => {
-                createFolderBlock(folder.name, files);
+                if (folderConfig === 'all') {
+                    files.sort((a, b) => a.name.localeCompare(b.name)); // Sort files by name
+                } else {
+                    // Filter files based on config
+                    files = files.filter(file => folderConfig.includes(file.name));
+                    files.sort((a, b) => a.name.localeCompare(b.name));
+                }
+                return createFolderBlock(folderName, files, folderConfig);
             })
-            .catch(error => console.error('Error fetching files:', error));
+            .catch(error => console.error('Error fetching files in folder:', error));
     });
+
+    // Process files that are not in any folder
+    const files = data.filter(item => item.type === 'file' && fileNames.includes(item.name));
+    createFileBlocks(files);
+
+    // Wait for all folder data to be processed
+    Promise.all(folderPromises)
+        .catch(error => console.error('Error processing folders:', error));
 }
 
-function createFolderBlock(folderName, files) {
+function createFolderBlock(folderName, files, folderConfig) {
     const contentContainer = document.querySelector('.accordion-container');
     if (!contentContainer) {
         console.error('Element .accordion-container not found');
@@ -71,36 +106,10 @@ function createFolderBlock(folderName, files) {
     const contentDiv = document.createElement('div');
     contentDiv.className = 'content';
 
-    files.forEach(file => {
-        if (file.type === 'file') {
-            const contentWrapperDiv = document.createElement('div');
-            contentWrapperDiv.className = 'content-wrapper';
-
-            const fileTitle = document.createElement('p');
-            fileTitle.className = 'index__our-values__carousel-title index__our-values__carousel-title-small';
-            fileTitle.innerHTML = `<img src="../img/education/${file.name}" alt="">${file.name}`;
-
-            const contentBtnsDiv = document.createElement('div');
-            contentBtnsDiv.className = 'content-btns';
-
-            const viewButton = document.createElement('a');
-            viewButton.className = 'education__buttons';
-            viewButton.href = file.download_url;
-            viewButton.target = '_blank';
-            viewButton.textContent = 'Ознайомитись';
-
-            const downloadButton = document.createElement('a');
-            downloadButton.className = 'education__buttons';
-            downloadButton.href = file.download_url;
-            downloadButton.download = '';
-            downloadButton.textContent = 'Завантажити';
-
-            contentBtnsDiv.appendChild(viewButton);
-            contentBtnsDiv.appendChild(downloadButton);
-            contentWrapperDiv.appendChild(fileTitle);
-            contentWrapperDiv.appendChild(contentBtnsDiv);
-            contentDiv.appendChild(contentWrapperDiv);
-        }
+    // Process files based on folderConfig
+    const sortedFiles = folderConfig === 'all' ? files : files.filter(file => folderConfig.includes(file.name));
+    sortedFiles.forEach((file, index) => {
+        setTimeout(() => createFileBlock(file, contentDiv), index * 500); // 500ms delay between files
     });
 
     setDiv.appendChild(titleLink);
@@ -108,7 +117,63 @@ function createFolderBlock(folderName, files) {
     contentContainer.appendChild(setDiv);
 }
 
+function createFileBlock(file, container) {
+    const contentWrapperDiv = document.createElement('div');
+    contentWrapperDiv.className = 'content-wrapper';
+
+    // Отримати відформатовану назву файлу
+    const formattedFileName = formatFileName(file.name);
+
+    const fileTitle = document.createElement('p');
+    fileTitle.className = 'index__our-values__carousel-title index__our-values__carousel-title-small';
+    fileTitle.innerHTML = `<img src="../img/education/${file.name}" alt="">${formattedFileName}`;
+
+    const contentBtnsDiv = document.createElement('div');
+    contentBtnsDiv.className = 'content-btns';
+
+    const viewButton = document.createElement('a');
+    viewButton.className = 'education__buttons';
+    viewButton.href = file.download_url;
+    viewButton.target = '_blank';
+    viewButton.textContent = 'Ознайомитись';
+
+    const downloadButton = document.createElement('a');
+    downloadButton.className = 'education__buttons';
+    downloadButton.href = file.download_url;
+    downloadButton.download = '';
+    downloadButton.textContent = 'Завантажити';
+
+    contentBtnsDiv.appendChild(viewButton);
+    contentBtnsDiv.appendChild(downloadButton);
+    contentWrapperDiv.appendChild(fileTitle);
+    contentWrapperDiv.appendChild(contentBtnsDiv);
+    container.appendChild(contentWrapperDiv);
+}
+
+function createFileBlocks(files) {
+    const contentContainer = document.querySelector('.accordion-container');
+    if (!contentContainer) {
+        console.error('Element .accordion-container not found');
+        return;
+    }
+
+    files.forEach((file, index) => {
+        setTimeout(() => {
+            const setDiv = document.createElement('div');
+            setDiv.className = 'set';
+
+            const contentDiv = document.createElement('div');
+            contentDiv.className = 'content';
+
+            createFileBlock(file, contentDiv);
+
+            setDiv.appendChild(contentDiv);
+            contentContainer.appendChild(setDiv);
+        }, index * 500); // 500ms delay between files
+    });
+}
+
 // Завантажити дані для поточної сторінки
 fetchWithCache(apiUrl)
-    .then(data => processAllData(data))
+    .then(data => processData(data))
     .catch(error => console.error('Error fetching all data:', error));
